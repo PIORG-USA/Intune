217fbc0905d0f64ca36de7038c550e746f0e7110
PioneersAllan <allan.mclaughlin@orlandoteam.com>
2024-04-04 12:15:20 -0400

    Add files via upload
    initial add

---
+++ IntuneAdd.ps1
@@ -0,0 +1,4 @@
+PowerShell.exe -ExecutionPolicy Bypass
+Install-Script -name Get-WindowsAutopilotInfo -Force
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+get-windowsautopilotinfo.ps1 -online -TenantID 5209a0b8-e194-4317-b4fa-e63c83391007 -appid dfee8a0a-e32a-4e1a-b712-fee5197f7ec7 -appsecret NHL8Q~1Fw1sghYWKnCtKVFzGQSXD9BZfxs4G3bUs
